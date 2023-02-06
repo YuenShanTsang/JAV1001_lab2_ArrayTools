e5640c72f8a86099f0b94954e39a61b803965fe6
Yuen Shan Tsang <A00270323@mycambrian.ca>
2023-02-06 11:35:46 -0500

    created .kt file

---
+++ ArrayTools.kt
@@ -0,0 +1,16 @@
+//Yuen Shan Tsang (A00270323)
+//JAV-1001 - 11354 - App Development for Android - 202301 - 001
+//Lab 2 - Functions and Arrays
+
+
+/* By collecting user input, the function should print the results.
+
+encrypt: A Caesar cipher function that accepts a String (the String to encrypt), a shift value, and returns the new String.
+
+arrayAvg: A function that accepts an array and produces the average of all values (array should be numeric type)
+
+arrayContains: A function that accepts an array and a search value, and determines if the array contains the value (true/false) (the array can use any data type you want)
+
+reverse: A function that reverses an array (gives a new array) (the array can use any data type you want)
+*/
+
